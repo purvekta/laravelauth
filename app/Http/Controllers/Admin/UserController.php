5247b03895098557f29fa45cff98aa4ed17e9c50
purvekta <purvekta@gmail.com>
2021-06-05 18:08:03 +0530

    final v2

---
+++ app/Http/Controllers/Admin/UserController.php
@@ -0,0 +1,111 @@
+<?php
+
+namespace App\Http\Controllers\Admin;
+use App\Models\User;
+use App\Models\Role;
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Yoeunes\Toastr\ToastrServiceProvider;
+
+class UserController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+        $users =User::all();
+        $roles = Role::all();
+        return view('admin.users.index',compact('users','roles'));
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        $user= User::findOrFail($id);
+        if(Auth::user()->id==$id){
+            toastr()->warning('Not Authorized change role ');
+return back();
+       
+        }
+        //
+        
+        $user->role_id = $request->role;
+        
+        $user->save();
+        toastr()->success('Role is changed!');
+return back();
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //
+        $user= User::findOrFail($id);
+        if(Auth::user()->id==$id){
+            toastr()->warning('Not Authorized to delete ');
+            return back();   
+        }
+        $user->delete();
+        toastr()->success('user deleted ');
+        return back();
+    }
+}
